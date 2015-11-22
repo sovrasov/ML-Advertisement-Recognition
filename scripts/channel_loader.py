b3ff3fe6d7f12a73c1d4d9410b178b24d3e09022
Sovrasov Vlad <vlads247@gmail.com>
2015-11-22 17:47:56 +0300

    Add function get_small_data into channel loader. This function reduces fiatures with zero variation.

--- scripts/channel_loader.py
+++ scripts/channel_loader.py
@@ -1,8 +1,15 @@
 from sklearn.externals.joblib import Memory
 from sklearn.datasets import load_svmlight_file
+from sklearn import feature_selection
 mem = Memory("../Dataset")
 
 @mem.cache
+def get_small_data(channel):
+    data = load_svmlight_file("../Dataset/" + channel + ".txt")
+    selector = feature_selection.VarianceThreshold()
+    X = selector.fit_transform(data[0])
+    return X, data[1]
+
 def get_data(channel):
     data = load_svmlight_file("../Dataset/" + channel + ".txt")
     return data[0], data[1]
